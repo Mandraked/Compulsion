c9c3bc1f8b613cec2badd94d8e7cbdfe69191d47
Brian Markham <Brian@Brian-PC.local>
2015-04-13 15:34:54 -0400

    Added sink

--- Assets/Scripts/LightControl.cs
+++ Assets/Scripts/LightControl.cs
@@ -8,10 +8,12 @@ using VibrationType = Thalmic.Myo.VibrationType;
 
 public class LightControl : MonoBehaviour {
 	public Light myLight;
+	public GameObject lSwitch = null;
 	public GameObject myo = null;
 	ThalmicMyo thalmicMyo = null;
 	private bool unlocked = false;
 	private Pose _lastPose = Pose.Unknown;
+	public float startingY = 0.3f;
 
 	// Use this for initialization
 	void Start () {
@@ -28,22 +30,18 @@ public class LightControl : MonoBehaviour {
 			if (thalmicMyo.pose == Pose.WaveIn && _lastPose != thalmicMyo.pose) {
 				_lastPose = thalmicMyo.pose;
 	        	Debug.Log("Wave In");
-				GameObject obj = GameObject.FindWithTag ("LightSwitch");
-				float val = 0.3f;
-				Vector3 vec = new Vector3 (obj.transform.localScale.x, val, obj.transform.localScale.z);
-				obj.transform.localScale = vec;
+				Vector3 vec = new Vector3 (lSwitch.transform.localScale.x, -1 * startingY, lSwitch.transform.localScale.z);
+				lSwitch.transform.localScale = vec;
 				myLight.enabled = false;
 			}
 			else if (thalmicMyo.pose == Pose.WaveOut && _lastPose != thalmicMyo.pose) {
 				_lastPose = thalmicMyo.pose;
 	        	Debug.Log("Wave Out");
-				GameObject obj = GameObject.FindWithTag ("LightSwitch");
-				float val = -0.3f;
-				Vector3 vec = new Vector3 (obj.transform.localScale.x, val, obj.transform.localScale.z);
-				obj.transform.localScale = vec;
+				Vector3 vec = new Vector3 (lSwitch.transform.localScale.x, startingY, lSwitch.transform.localScale.z);
+				lSwitch.transform.localScale = vec;
 				myLight.enabled = true;
 			}
-			else if (thalmicMyo.pose == Pose.Fist && _lastPose != thalmicMyo.pose) {
+			else if (thalmicMyo.pose == Pose.DoubleTap && _lastPose != thalmicMyo.pose) {
 				unlocked = false;
 				thalmicMyo.Lock ();
         		Debug.Log("Locking");
---
+++ Assets/Scripts/WaterControl.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class WaterControl : MonoBehaviour {
+	public ParticleSystem waterSystem;
+	public GameObject myo = null;
+	ThalmicMyo thalmicMyo = null;
+	private bool unlocked = false;
+	private Pose _lastPose = Pose.Unknown;
+
+	// Use this for initialization
+	void Start () {
+		waterSystem.enableEmission = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+
+		if (unlocked)
+		{
+			if (thalmicMyo.pose == Pose.WaveIn && _lastPose != thalmicMyo.pose) {
+				_lastPose = thalmicMyo.pose;
+	        	Debug.Log("Wave In");
+				waterSystem.enableEmission = true;
+			}
+			else if (thalmicMyo.pose == Pose.WaveOut && _lastPose != thalmicMyo.pose) {
+				_lastPose = thalmicMyo.pose;
+	        	Debug.Log("Wave Out");
+				waterSystem.enableEmission = false;
+			}
+			else if (thalmicMyo.pose == Pose.DoubleTap && _lastPose != thalmicMyo.pose) {
+				unlocked = false;
+				thalmicMyo.Lock ();
+        		Debug.Log("Locking");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+        thalmicMyo.Unlock (UnlockType.Hold);
+        Debug.Log("Unlocked");
+
+		unlocked = true;
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (thalmicMyo.unlocked == true) {
+			unlocked = false;
+			thalmicMyo.Lock ();
+	        Debug.Log("Locking");
+		}
+	}
+}
